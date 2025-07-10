45331de861d0ec13cd39d018b430ce6fc255ab38
Shanpan-1 <meshantanu3@gmail.com>
2025-07-09 22:14:11 -0400

    Update Lab 1.py

--- Lab 1.py
+++ Lab 1.py
@@ -6,13 +6,19 @@ class Biome:
         print(f"Hello I am on plant {self.place} Here are the biome I have.")
 
 class Ocean(Biome):
-    def __init__ (self, place, power):
+    def __init__ (self, place, water):
         super().__init__ (place)
-        self.power = power
+        self.water = water
 
     def on(self):
-        print(f"Hello I am on the planet {self.place} and my power is {self.power}")
+        print(f"Hello I am on the planet {self.place} and my power is {self.water}")
+class Dessert(Biome):
+    def __init (self, place, heat):
+        super().__init__ (place)
+        self.heat = heat
 
+    def on(self):
+        print(f"Hello I am the {self.place} biome and I have the {self.heat} power")
 class Sea_Animal(Ocean):
     def __init (self, place, power, type):
         super().__init__(place, power)
